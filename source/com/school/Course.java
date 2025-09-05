bfd6f9dd03b2464bdb3d8ac4055c6c1c1bdefd6d
Quick-Genius <singhharshgood@gmail.com>
2025-09-05 14:39:25 +0530

    made basic structure of this

---
+++ source/com/school/Course.java
@@ -0,0 +1,14 @@
+package com.school;
+public class Course {
+    private int courseId;
+    private String courseName;
+
+    public void setDetails(int courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public void displayDetails() {
+        System.out.println("Course ID: " + courseId + ", Course Name: " + courseName);
+    }
+}
